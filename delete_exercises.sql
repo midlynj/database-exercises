ecb4f3f78c2c7f6a9b77e961f12dd7e7ebde9dd6
midlynj <jmidlyn@gmail.com>
2022-08-25 16:58:58 -0500

    delete table row exercise complete

---
+++ delete_exercises.sql
@@ -0,0 +1,20 @@
+SELECT record_name FROM albums WHERE release_date >1991;
+--Carter III
+--Luv Is Rage 2
+--Rolling Papers
+--The Bodyguard
+--Come On Over
+--Jagged Little Pill
+--Falling into You
+--21
+--1
+--Let's Talk About Love
+--Gold: Greatest Hits
+--Supernatural
+
+
+SELECT record_name FROM albums WHERE artist ='Whitney Houston'
+--The Bodyguard
+
+SELECT artist FROM albums WHERE artist ='Lil Wayne'
+DELETE FROM albums WHERE artist = 'Lil Wayne';
