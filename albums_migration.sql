d0adcf4063287c8234cd85e3ef078022a08e1287
midlynj <jmidlyn@gmail.com>
2022-08-25 11:23:50 -0500

    album table created

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,11 @@
 USE codeup_test_db;
+
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50),
+                        record_name  VARCHAR(100) NOT NULL,
+                        release_date int NOT NULL,
+                        sales float,
+                        genre VARCHAR(30)
+                        PRIMARY KEY (id)
+);
