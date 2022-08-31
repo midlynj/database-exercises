27ef89e04ad7994429d547a40f7d041f0bdc03e2
midlynj <jmidlyn@gmail.com>
2022-08-31 17:25:04 -0500

    aliases exercise complete

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,14 @@
 use employees;
 
+SELECT CONCAT_WS(' ',last_name, first_name) as full_name
+FROM employees
+LIMIT 10;
+
+SELECT CONCAT_WS(' ',last_name, first_name) as full_name, birth_date as DOB
+FROM employees
+LIMIT 10;
+
+
 SELECT  CONCAT_WS(' ', emp_no, last_name, first_name) as full_name, birth_date as DOB
 FROM employees
 LIMIT 10;
