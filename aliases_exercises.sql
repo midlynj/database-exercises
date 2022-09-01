dcf31fa743ed5482dc4830a61226395d30a660bb
midlynj <jmidlyn@gmail.com>
2022-09-01 11:20:00 -0500

    adding unique keys in tables

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -13,3 +13,15 @@ SELECT  CONCAT_WS(' ', emp_no, last_name, first_name) as full_name, birth_date a
 FROM employees
 LIMIT 10;
 
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_manager;
+DESCRIBE employees;
+DESCRIBE salaries;
+DESCRIBE titles;
+
+use midlyn;
+DESCRIBE albums;
+DESCRIBE pets;
+DESCRIBE quotes;
+
