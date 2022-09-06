f4bd7a93135f2ce7e9159855c671155b4ac91fa9
midlynj <jmidlyn@gmail.com>
2022-09-06 11:01:20 -0500

    where exercise complete

---
+++ out/production/database-exercises/where_exercises.sql
@@ -0,0 +1,42 @@
+use employees;
+
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena','Vidya', 'Maya');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%e';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT first_name
+FROM employees
+WHERE (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya');
+
+SELECT first_name
+FROM employees
+WHERE (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya')
+AND gender = 'M';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
+
